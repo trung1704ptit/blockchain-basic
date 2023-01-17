1b4eb792ec58362a5a1e63669df57d91624fcea6
Trung Nguyen <trung1704ptit@gmail.com>
2023-01-17 21:25:47 +0800

    update code

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
-const Block = require("./block");
-const cryptoHash = require("./crypto-hash");
+const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
   constructor() {
@@ -8,8 +8,8 @@ class Blockchain {
 
   addBlock({ data }) {
     const newBlock = Block.mineBlock({
-      lastBlock: this.chain[this.chain.length - 1],
-      data,
+      lastBlock: this.chain[this.chain.length-1],
+      data
     });
 
     this.chain.push(newBlock);
@@ -17,37 +17,36 @@ class Blockchain {
 
   replaceChain(chain) {
     if (chain.length <= this.chain.length) {
-      console.error("The incoming chain must be longer");
+      console.error('The incoming chain must be longer');
+
       return;
     }
 
     if (!Blockchain.isValidChain(chain)) {
-      console.error("The incoming chain must be valid");
+      console.error('The incoming chain must be valid');
+
       return;
     }
 
-    console.log("replacing the chain");
+    console.log('replacing chain wiih', chain);
     this.chain = chain;
   }
 
   static isValidChain(chain) {
-    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
-      return false;
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
 
-    for (let i = 1; i < chain.length; i++) {
-      const { timestamp, lastHash, hash, data, nonce, difficulty } = chain[i];
+    for (let i=1; i<chain.length; i++) {
+      const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
+      const actualLastHash = chain[i-1].hash;
       const lastDifficulty = chain[i-1].difficulty;
-
-      const actualLastHash = chain[i - 1].hash;
 
       if (lastHash !== actualLastHash) return false;
 
       const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
       if (hash !== validatedHash) return false;
-      if (Math.abs(lastDifficulty - difficulty) > 1) {
-        return false;
-      }
+
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
     }
 
     return true;
