b3cd337785847f141e3c325767a17550eaf00127
Trung Nguyen <trung1704ptit@gmail.com>
2023-01-24 23:00:27 +0800

    update pubsub

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-const express = require('express');
-const bodyParser =  require('body-parser');
-const Blockchain =  require('./blockchain');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+const DEFAULT_PORT = 3000;
 
-app.use(bodyParser.json())
+const ROOT_NODE_ADDRESSES = `http://localhost:${DEFAULT_PORT}`;
 
-app.get('/api/blocks', (req, res) => {
-    res.json(blockchain.chain);
-})
+app.use(bodyParser.json());
 
-app.post('/api/mine', (req, res) => {
-    const { data } = req.body;
-    blockchain.addBlock({ data })
-    res.redirect('/api/blocks')
-})
+app.get("/api/blocks", (req, res) => {
+  res.json(blockchain.chain);
+});
 
+app.post("/api/mine", (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+  res.redirect("/api/blocks");
+});
 
-const PORT = 3000;
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESSES}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
 
-app.listen(3000, () => {
-    console.log(`listening in localhost:${PORT}`);
-})
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.log(`listening in localhost:${PORT}`);
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
+});
