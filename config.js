7480608a4eaa351b80dbf2d01c34f589ed25da68
Trung Nguyen <trung1704ptit@gmail.com>
2023-01-29 12:19:22 +0800

    add transaction

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ const INITIAL_DIFFICULTY = 3;
 
 const GENESIS_DATA = {
   timestamp: 1,
-  lastHash: '-----',
-  hash: 'hash-one',
+  lastHash: "-----",
+  hash: "hash-one",
   difficulty: INITIAL_DIFFICULTY,
   nonce: 0,
-  data: []
+  data: [],
 };
 
-module.exports = { GENESIS_DATA, MINE_RATE };
+const STARTING_BALANCE = 100;
+
+module.exports = { GENESIS_DATA, MINE_RATE, STARTING_BALANCE };
